3784976b2b972836cede98ef53275e62d27fddb8
Miguel <miguelviana@gmail.com>
2025-11-13 11:33:40 -0300

    visualização

---
+++ view/visualizarAdmin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil Empresarial</title>
+    <link rel="stylesheet" href="../css/views/style.css">
+</head>
+<body>
+    <div class="auth-container">
+        <h1 class="auth-title">Perfil Adminl</h1>
+
+        <table>
+            <tr>
+                <th>Nome da Empresa</th>
+                <td>TechVision Ltda</td>
+            </tr>
+            <tr>
+                <th>E-mail</th>
+                <td>contato@techvision.com.br</td>
+            </tr>
+            <tr>
+                <th>Telefone</th>
+                <td>(21) 98765-4321</td>
+            </tr>
+        </table>
+
+        <div class="auth-footer">
+            <a href="cadastro_empresarial.html">Editar Cadastro</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ view/visualizarCriador.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil do Visitante</title>
+    <link rel="stylesheet" href="../css/views/style.css">
+</head>
+<body>
+    <div class="auth-container">
+        <h1 class="auth-title">Perfil do Criador</h1>
+
+        <table>
+            <tr>
+                <th>Nome</th>
+                <td>João Silva</td>
+            </tr>
+            <tr>
+                <th>E-mail</th>
+                <td>joao.silva@example.com</td>
+            </tr>
+        </table>
+
+        <div class="auth-footer">
+            <a href="cadastro_visitante.html">Editar Cadastro</a>
+        </div>
+    </div>
+</body>
+</html>
